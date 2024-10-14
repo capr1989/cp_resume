e17783c7a3486d23afe73cd96d367831723cada9
Carlos Pretto <carlospretto1989@gmail.com>
2024-10-14 18:55:52 -0500

    fixwd errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,37 +12,41 @@ import purgecss from "astro-purgecss";
 
 import compressor from "astro-compressor";
 
-import frontendistahtmlMinify from "@frontendista/astro-html-minify";
-
 // https://astro.build/config
 export default defineConfig({
   build: {
     inlineStylesheets: "never",
   },
-  integrations: [react(), tailwind({
-    applyBaseStyles: false,
-  }), sitemap(), compressor({
-    fileExtensions: [
-      ".html",
-      ".js",
-      ".jsx",
-      ".ts",
-      ".tsx",
-      ".css",
-      ".xml",
-      ".cjs",
-      ".mjs",
-      ".svg",
-    ],
-  }), purgecss({
-    extractors: [
-      {
-        extractor: (content) =>
-          content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
-        extensions: ["astro", "html", "js", "jsx", "ts", "tsx"],
-      },
-    ],
-  }), frontendistahtmlMinify()],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+    compressor({
+      fileExtensions: [
+        ".html",
+        ".js",
+        ".jsx",
+        ".ts",
+        ".tsx",
+        ".css",
+        ".xml",
+        ".cjs",
+        ".mjs",
+        ".svg",
+      ],
+    }),
+    purgecss({
+      extractors: [
+        {
+          extractor: (content) =>
+            content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
+          extensions: ["astro", "html", "js", "jsx", "ts", "tsx"],
+        },
+      ],
+    }),
+  ],
   site: "https://carlospretto.com",
 
   output: "hybrid",
