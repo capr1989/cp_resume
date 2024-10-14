0ee8bd52297dfbf4fd843a2ce3d016817105e002
Carlos Pretto <carlospretto1989@gmail.com>
2024-10-14 18:45:12 -0500

    minified

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,41 +12,37 @@ import purgecss from "astro-purgecss";
 
 import compressor from "astro-compressor";
 
+import frontendistahtmlMinify from "@frontendista/astro-html-minify";
+
 // https://astro.build/config
 export default defineConfig({
   build: {
     inlineStylesheets: "never",
   },
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    sitemap(),
-    compressor({
-      fileExtensions: [
-        ".html",
-        ".js",
-        ".jsx",
-        ".ts",
-        ".tsx",
-        ".css",
-        ".xml",
-        ".cjs",
-        ".mjs",
-        ".svg",
-      ],
-    }),
-    purgecss({
-      extractors: [
-        {
-          extractor: (content) =>
-            content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
-          extensions: ["astro", "html", "js", "jsx", "ts", "tsx"],
-        },
-      ],
-    }),
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+  }), sitemap(), compressor({
+    fileExtensions: [
+      ".html",
+      ".js",
+      ".jsx",
+      ".ts",
+      ".tsx",
+      ".css",
+      ".xml",
+      ".cjs",
+      ".mjs",
+      ".svg",
+    ],
+  }), purgecss({
+    extractors: [
+      {
+        extractor: (content) =>
+          content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
+        extensions: ["astro", "html", "js", "jsx", "ts", "tsx"],
+      },
+    ],
+  }), frontendistahtmlMinify()],
   site: "https://carlospretto.com",
 
   output: "hybrid",
